a71a40f94576ada023829cd9c4657f07a0df1994
skim6281 <skim6281@gmail.com>
2017-06-27 15:57:59 -0400

    Update root action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,11 @@
 class StaticPagesController < ApplicationController
+  require 'rubygems'
+  require 'gems'
 
   def root
+    @gem = Gems.search params[:name]
+    @dependencies = Gems.dependencies @gem if @gem
+    render 'ruby_gems/show'
   end
 
   def favorites
