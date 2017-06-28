d909827aa547908f1f1feccb3cf0373e78436501
skim6281 <skim6281@gmail.com>
2017-06-28 18:29:30 -0400

    Add dependencies action

--- app/controllers/gem_controller.rb
+++ app/controllers/gem_controller.rb
@@ -1,15 +1,22 @@
 class GemController < ApplicationController
-
   require 'rubygems'
   require 'gems'
 
   def show
     @gem = Gems.search params[:name]
     if @gem
-      @dependencies = Gems.dependencies params[:name]
-      render 'ruby_gems/gem'
+      render 'ruby_gems/search'
     else
-      render json: ["Gem not found"], status: 422
+      render json; ["No gem found"]
+    end
+  end
+
+  def dependencies
+    @gems = Gems.dependencies params[:name]
+    if @gems
+      render 'ruby_gems/dependencies'
+    else
+      render json: ["No dependencies"]
     end
   end
 end
