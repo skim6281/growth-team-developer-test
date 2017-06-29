383f287433d843dcefc9eb7571e986591a7e4cc5
skim6281 <skim6281@gmail.com>
2017-06-29 16:03:37 -0400

    Remove unnecessary text

--- frontend/components/nav.jsx
+++ frontend/components/nav.jsx
@@ -1,7 +1,20 @@
+import React from 'react';
+
 const Nav = props => {
   return (
-    <div>
-      
-    </div>
+    <section className="nav-container">
+      <div className="map-container">
+        <img src={window.images.map} />
+        <div className="logo-container">
+          <img src={window.images.logo} />
+        </div>
+        <ul>
+          <li>Search</li>
+          <li>Favorites</li>
+        </ul>
+      </div>
+    </section>
   )
 }
+
+export default Nav;
