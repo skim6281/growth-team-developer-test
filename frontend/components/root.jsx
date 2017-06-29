c6a11d59827e3c75851a1027bcc7d3d86415df52
skim6281 <skim6281@gmail.com>
2017-06-29 13:17:58 -0400

    Complete root

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,3 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Router, Route} from 'react-router';
+import { Router, Route } from 'react-router';
+import App from './app';
+
+const Root = ({store}) => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+}
+
+export default Root;
