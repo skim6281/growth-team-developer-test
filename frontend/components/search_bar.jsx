1051776ece8953e8c3aef4597ba24511249915ce
skim6281 <skim6281@gmail.com>
2017-06-30 19:24:39 -0400

    Add submit method

--- frontend/components/search_bar.jsx
+++ frontend/components/search_bar.jsx
@@ -1,19 +1,45 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchGem } from '../action.js';
 
 class SearchBar extends React.Component {
   constructor(props) {
-    const state = {
-      query: "";
+    super(props);
+    this.state = {
+      query: ""
     }
+    this.update = this.update.bind(this);
+    this.submit = this.submit.bind(this);
   }
 
+  update(e) {
+    e.preventDefault();
+    this.setState({query: e.target.value});
+  }
 
+  submit(e) {
+    e.preventDefault();
+    this.fetchGem(thi.state.query);
+  }
 
   render() {
-    <form onSubmit={}>
-
+    <form className='search-bar-container' onSubmit={this.submit}>
+      <input
+        className='search-input'
+        placeholder='Search'
+        onChange={this.update}
+        />
     </form>
   }
 }
 
-export default SearchBar;
+const mapDispatchToProps = dispatch => {
+  return ({
+    fetchGem: name => dispatch(fetchGem())
+  });
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SearchBar);
