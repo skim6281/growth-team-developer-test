d20f06d5db7dd87affc106c36b3bce0b0f9b83d6
skim6281 <skim6281@gmail.com>
2017-06-30 18:56:53 -0400

    Add fetchGem method

--- frontend/action.js
+++ frontend/action.js
@@ -1,6 +1,16 @@
-const fetchGem = (name) => {
+const fetchRubyGem = name => {
   return $.ajax({
     method: 'GET',
     url: `/gem/${name}`
   });
+};
+
+export const receiveGem = gem => ({
+  type: 'RECEIVE_GEM',
+  gem
+});
+
+export const fetchGem = name => dispatch => {
+  return fetchRubyGem(name)
+    .then(gem => dispatch(receiveGem(gem)));
 };
