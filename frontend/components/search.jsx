833de36585d9d2390ac88f63a0cb0219775397dc
skim6281 <skim6281@gmail.com>
2017-06-30 19:24:16 -0400

    Add super in constructor

--- frontend/components/search.jsx
+++ frontend/components/search.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import SearchBar from './search_bar';
 
 class Search extends React.Component {
   constructor(props) {
-
+    super(props);
   }
 
   render() {
-    
+    return (
+      <content>
+        SearchComponent
+      </content>
+    );
   }
 
 }
