21d5f5d5290d258de8e096fd672d1f6a1f78ab24
skim6281 <skim6281@gmail.com>
2017-06-30 19:25:08 -0400

    Add IndexRoute

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Router} from 'react-router';
+import { Router, IndexRoute} from 'react-router';
 import { BrowserRouter, Route } from 'react-router-dom';
 import App from './app';
+import Search from './search';
 
 const Root = ({store}) => {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Route path='/' component={App}>
+          <IndexRoute component={Search} />
         </Route>
       </BrowserRouter>
     </Provider>
