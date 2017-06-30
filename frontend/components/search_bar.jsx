80f38cf3e850c73ea463880a3315512005106238
skim6281 <skim6281@gmail.com>
2017-06-30 18:50:54 -0400

    Add search bar component

---
+++ frontend/components/search_bar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    const state = {
+      query: "";
+    }
+  }
+
+
+
+  render() {
+    <form onSubmit={}>
+
+    </form>
+  }
+}
+
+export default SearchBar;
