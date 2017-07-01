8786252e4a2aaf60649e8e8a50f708b3d09d2102
skim6281 <skim6281@gmail.com>
2017-06-30 20:39:55 -0400

    Remove space

--- frontend/gemsearch.jsx
+++ frontend/gemsearch.jsx
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom';
 import Reducer from './reducer';
 import Root from './components/root';
 
-
 const configureStore = (preloadedState = {}) => (
   createStore(
     Reducer,
