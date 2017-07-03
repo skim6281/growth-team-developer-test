512b4fb15e89bf1dedebe41f75645e8ec6a2b7df
skim6281 <skim6281@gmail.com>
2017-07-03 14:23:27 -0400

    Comment out favorites route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: "static_pages#root"
-  get '/favorites' => "static_pages#favorites"
+  root "static_pages#root"
+  # get '/favorites' => "static_pages#favorites"
 
   resources :gem, defaults: { format: :json }, param: :name, only: [:show]
 end
