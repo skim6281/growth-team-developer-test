c440d074ebaa3e402cdc62098832b3b3d928a48a
skim6281 <skim6281@gmail.com>
2017-07-03 19:11:50 -0400

    Add renderTabs method

--- frontend/components/nav.jsx
+++ frontend/components/nav.jsx
@@ -1,21 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Nav = props => {
-  return (
-    <aside className="nav-container">
-      <div className="map-container">
-        <img src={window.images.map} />
-        <div className="logo-container">
-          <img src={window.images.logo} />
+class Nav extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  renderTabs() {
+    const location = window.location.hash;
+    if(location === "#/favorites") {
+      return(
+        <section>
+          <div><Link className="link" to="/" >Search</Link></div>
+          <div>Favorites</div>
+          <div className="fave-bg bg"></div>
+        </section>
+      );
+    } else {
+      return(
+        <section>
+          <div>Search</div>
+          <div className="search-bg bg"></div>
+          <div><Link className="link" to="/favorites">Favorites</Link></div>
+        </section>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <aside className="nav-container">
+        <div className="map-container">
+          <img src={window.images.map} />
+          <div className="logo-container">
+            <img src={window.images.logo} />
+          </div>
+            {this.renderTabs()}
         </div>
-        <ul>
-          <li><Link className="link" to="/" >Search</Link></li>
-          <li><Link className="link" to="/favorites">Favorites</Link></li>
-        </ul>
-      </div>
-    </aside>
-  )
+      </aside>
+    );
+  }
 }
 
 export default Nav;
