c7238fc991d483dfea595ba51bdc8d6f6624b9ae
skim6281 <skim6281@gmail.com>
2017-07-03 14:29:15 -0400

    Add Gem component

---
+++ frontend/components/gem.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { addFavorite, deleteFavorite } from '../action';
+
+class Gem extends React.Component{
+  constructor(props) {
+    super(props);
+    this.handleFave = this.handleFave.bind(this);
+    this.handleUnfave = this.handleUnfave.bind(this);
+  }
+
+  handleFave(name) {
+    return () => this.props.addFavorite(name);
+  }
+
+  handleUnfave(name) {
+    return () => this.props.deleteFavorite(name);
+  }
+
+  renderStar(name) {
+    if(this.props.favorites.includes(name)) {
+      return(
+        <img className="star" src={window.images.starBlue} onClick={this.handleUnfave(name)}/>
+      )
+    } else {
+      return(
+        <img className="star" src={window.images.starGray} onClick={this.handleFave(name)}/>
+      )
+    }
+  }
+
+  render() {
+    const { name, search } = this.props;
+    if(search) {
+      return(
+        <div className="gem-link">
+          <a href={`https://rubygems.org/gems/${name}`} target="_blank">{name}</a>
+          {this.renderStar(name)}
+        </div>
+      )
+    } else {
+      return(
+        <div className="gem-link">
+          {this.renderStar(name)}
+          <a href={`https://rubygems.org/gems/${name}`} target="_blank">{name}</a>
+        </div>
+      )
+    }
+
+  }
+}
+
+const mapStateToProps = state => {
+  return ({
+    favorites: state.favorites.favorites
+  });
+}
+
+const mapDispatchToProps = dispatch => {
+  return ({
+    addFavorite: gemName => dispatch(addFavorite(gemName)),
+    deleteFavorite: gemName => dispatch(deleteFavorite(gemName))
+  });
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Gem);
