ba169c94b945839f6e1da24d91c93d7cdb00e4a7
skim6281 <skim6281@gmail.com>
2017-07-02 22:08:33 -0400

    Render gem name, info and dependencies

--- frontend/components/gem_detail.jsx
+++ frontend/components/gem_detail.jsx
@@ -4,28 +4,54 @@ import { connect } from 'react-redux';
 class GemDetail extends React.Component {
   constructor(props) {
     super(props);
+    this.renderDependencies = this.renderDependencies.bind(this);
+  }
+
+  renderDependencies(gems) {
+    if(gems.length === 0) {
+      return (None);
+    }
+    return (
+      <ul>
+        {gems.map((gem,index) => <li key={index}>{gem.name}</li>)}
+      </ul>
+    )
+
   }
 
   render() {
     const { gem } = this.props;
-    return (
-      <content>
-        <div className="gem-name">
-          { gem.name }
-        </div>
-        <div>
-
-        </div>
-
-      </content>
-    )
+    console.log("gem: "+gem);
+    if(gem) {
+      return (
+        <content>
+          <div className="gem-name">
+            { gem.name }
+          </div>
+          <div className="gem-info">
+            { gem.info }
+          </div>
+          <div>
+            {this.renderDependencies(gem.dependencies)}
+          </div>
+        </content>
+      )
+    } else {
+      return(
+        <content>
+          <div>
+            No gem found
+          </div>
+        </content>
+      )
+    }
   }
 
 }
 
 const mapStateToProps = state => {
   return ({
-    gem: state
+    gem: state.gem
   });
 };
 
