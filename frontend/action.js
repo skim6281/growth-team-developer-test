bb4d8e6e80eff394fe96babd479d129b61aa9235
skim6281 <skim6281@gmail.com>
2017-07-03 14:24:24 -0400

    Add fetchFavorites, addFavorite, deleteFavorite

--- frontend/action.js
+++ frontend/action.js
@@ -5,12 +5,52 @@ export const fetchRubyGem = name => {
   });
 };
 
+export const fetchFavorites = () => {
+  const gems = [];
+  for(let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    if(key.includes('fave-gem')) {
+      gems.push(localStorage.getItem(key));
+    }
+  }
+  return {
+    type: 'RECEIVE_FAVORITES',
+    favorites: gems
+  }
+}
+
 export const receiveGem = gem => ({
   type: 'RECEIVE_GEM',
   gem
+});
+
+export const receiveFavorite = gemName => ({
+  type: 'RECEIVE_FAVORITE',
+  gemName
+});
+
+export const removeFavorite = gemName => ({
+  type: 'REMOVE_FAVORITE',
+  gemName
 });
 
 export const fetchGem = name => dispatch => {
   return fetchRubyGem(name)
     .then(gem => dispatch(receiveGem(gem)));
 };
+
+export const addFavorite = name => dispatch => {
+  let key = `fave-gem-${name}`;
+  if(!window.localStorage.hasOwnProperty(key)) {
+    window.localStorage.setItem(key, name);
+    dispatch(receiveFavorite(name));
+  }
+}
+
+export const deleteFavorite = name => dispatch => {
+  let key = `fave-gem-${name}`;
+  if(window.localStorage.hasOwnProperty(key)) {
+    window.localStorage.removeItem(key);
+    dispatch(removeFavorite(name));
+  }
+}
