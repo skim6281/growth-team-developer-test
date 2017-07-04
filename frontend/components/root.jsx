a71521bc20e4701d6bd0768eed0bc165df518f1e
skim6281 <skim6281@gmail.com>
2017-07-04 10:13:08 -0400

    Remove unused imports

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Router, IndexRoute, Switch } from 'react-router';
-import { BrowserRouter, HashRouter, Route } from 'react-router-dom';
+import { Switch } from 'react-router';
+import { HashRouter, Route } from 'react-router-dom';
 import App from './app';
 import Search from './search';
 import Favorites from './favorites';
